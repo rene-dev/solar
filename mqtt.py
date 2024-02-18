dfe07ce4c05c90ed325bd46f4a49033bc4a68914
Rene Hopf <renehopf@mac.com>
2024-02-18 18:53:39 +0100

    systemd service

--- mqtt.py
+++ mqtt.py
@@ -3,13 +3,13 @@ import paho.mqtt.client as mqtt
 from time import sleep
 import json
 import socket
+from contextlib import ExitStack
 
 battery = {}
 power = 0.0
-
-#rechts
-HOST = "192.168.177.89"  # The server's hostname or IP address
-PORT = 8000  # The port used by the server
+rse_limit = 100
+rse_limit_send = 0
+kwp_total = 29580
 
 def crc16(data):
   crc=0x0
@@ -20,18 +20,24 @@ def crc16(data):
   return crc
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe([("pylontech/#",0),("trifasipower",0)])
+    client.subscribe([("pylontech/#",0),("trifasipower",0),("rundfunk/scale",0)])
 
 def on_message(client, userdata, msg):
     global power
     global battery
+    global rse_limit
     if msg.topic == "trifasipower":
         power = json.loads(msg.payload)['Q']['total']
-        print(power)
+        #print(power)
     elif 'pylontech' in msg.topic:
         #print(f'{msg.topic=} {msg.payload=}')
         battery[msg.topic.split('/')[2]] = float(msg.payload)
         #print(battery)
+        #print(msg.topic.split('/')[2]+" "+str(msg.payload))
+    elif 'rundfunk' in msg.topic:
+        #print(f'{msg.topic=} {msg.payload=}')
+        rse_limit = float(msg.payload)
+        #print(f'{rse_limit=}')
         #print(msg.topic.split('/')[2]+" "+str(msg.payload))
 
 client = mqtt.Client("solarfoo")
@@ -41,19 +47,36 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.loop_start()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as l, socket.socket(socket.AF_INET, socket.SOCK_STREAM) as r:
+    l.connect(('solar232links', 8000))
+    r.connect(('solar232rechts', 8000))
+    s = r
     sleep(5)
+
+    sleep(1)
     while True:
+      if rse_limit != rse_limit_send:
+        limit = min(10000,kwp_total/2.0 * rse_limit/100.0)
+        print(f'setting feed limit to {limit}')
+        rse_limit_send = rse_limit
+        #print("maxfeed")
+        cmd_maxfeed = f'^S011GPMP{int(limit):06}\r'  #  b'^1\x0b\xc2\r'
+        s.sendall(cmd_maxfeed.encode())
+        print(s.recv(1024))
+        l.sendall(cmd_maxfeed.encode())
+        print(l.recv(1024))
+        #print("maxfeed")
+
       max_feed = 10000
       cmd_feed = int(power)
       #print(cmd_feed)
       eminfo = f"^S026EMINFO00000,{max_feed:05},{1 if cmd_feed > 0 else 0},{abs(cmd_feed):05}\r"
-      print(eminfo)
+      #print(eminfo)
       s.sendall(eminfo.encode())
-      print(s.recv(1024))
+      reply = s.recv(1024)
+      #print(reply)
       sleep(0.5)
-      print("BMS:")
+      #print("BMS:")
       soc = int(battery['SOC'])
       volt = int(battery['voltage_module']*10)#422
       cd = 0 #charge/discharge
@@ -67,24 +90,24 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
       cv = int(battery['Chargevoltage']*10)
       force_charge = 0#geht b
       warning = 0
-      if int(battery['Chargelimit']) <= 10 or soc >= 99:
-        print('limiter')
+      if int(battery['Chargelimit']) <= 10 or soc >= 100:
+        print('battery full')
         stopch = 1
         maxchg = 0
       if int(battery['Dischargelimit']) <= 10 or soc <= 20:
-        print('limiter-')
+        print('battery low')
         stopdis = 1
         maxdis = 0
 
       cmd = f"^D054BMS{volt:04},{soc:03},{cd},{current:04},{warning:01},{force_charge},{cv:04},{floatv:04},{maxchg:04},{stopdis},{stopch},{cutoff:04},{maxdis:04}".encode()
-      print(f'{cmd=}')
+      #print(f'{cmd=}')
       crc = crc16(cmd)
       cmd = cmd + (crc >> 8).to_bytes(1) + (crc&0xff).to_bytes(1) + b'\r'
       s.sendall(cmd)
       sleep(0.5)
       s.sendall(b"^P004BMS\r")
       data = s.recv(1024)
-      print(f'{data=}')
+      #print(f'{data=}')
 
 
 
